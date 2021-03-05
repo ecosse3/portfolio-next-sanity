97d20714419341239cfb127496bd39f10121ca38
ecosse3 <theecosse@gmail.com>
2021-03-05 23:15:15 +0100

    Init eslint & typescript

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:react-hooks/recommended',
+    'airbnb',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+    'prettier',
+    '@typescript-eslint',
+  ],
+  rules: {
+    'prettier/prettier': 'error',
+  },
+};
